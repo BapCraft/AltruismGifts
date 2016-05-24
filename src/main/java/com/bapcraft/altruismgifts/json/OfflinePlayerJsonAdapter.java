08301b14ba105d62b283179e0d17328c81a232e7
treyzania <Treyzania@gmail.com>
2016-05-23 20:02:48 -0400

    Added the json adapter that I forgot to commit earlier.

---
+++ src/main/java/com/bapcraft/altruismgifts/json/OfflinePlayerJsonAdapter.java
@@ -0,0 +1,29 @@
+package com.bapcraft.altruismgifts.json;
+
+import java.lang.reflect.Type;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+
+public class OfflinePlayerJsonAdapter implements JsonSerializer<OfflinePlayer>, JsonDeserializer<OfflinePlayer> {
+
+	@Override
+	public JsonElement serialize(OfflinePlayer player, Type type, JsonSerializationContext ctx) {
+		return new JsonPrimitive(player.getUniqueId().toString());
+	}
+	
+	@Override
+	public OfflinePlayer deserialize(JsonElement ele, Type type, JsonDeserializationContext ctx) throws JsonParseException {
+		return Bukkit.getOfflinePlayer(UUID.fromString(ele.getAsString()));
+	}
+	
+}
